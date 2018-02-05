ef3b4f7a9e3d738ac39e15f933e2cec5bd6b54d0
ZiheLiu <ziheliu1024@gmail.com>
2018-02-05 19:35:13 +0800

    add test coverage

---
+++ src/Test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Test extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div className="container">
+        <div className="danger">hello</div>
+      </div>
+    );
+  }
+}
+
+export default Test;
